f82e53f90cf0faa99c7610be8cd1aed223f8f596
Sunghan Kim <80890062+s-ggul@users.noreply.github.com>
2024-09-12 17:26:54 +0900

    Title: 술래잡기, Time: 195ms, Memory: 12MB, Status: Passed - CodeTree

--- 240912/술래잡기/hide-and-seek.java
+++ 240912/술래잡기/hide-and-seek.java
@@ -32,6 +32,7 @@ public class Main {
 	static Pair[] thieves;
 	static Pair[] trees;
 	static Pair tagger;
+	static int[][] dir;
 	static int[][] dxy = {{-1,0},{0,1},{1,0},{0,-1}};
 	static int[][] rdxy = {{1,0},{0,1},{-1,0},{0,-1}};
 	public static void main(String[] args) throws IOException{
@@ -45,7 +46,7 @@ public class Main {
 
 		thieves = new Pair[M];
 		trees = new Pair[H];
-		
+
 		answer = 0;
 
 		// 술래는 가운데서 시작하고 
@@ -74,12 +75,12 @@ public class Main {
 			st = new StringTokenizer(br.readLine());
 			trees[i] = new Pair(Integer.parseInt(st.nextToken())-1, Integer.parseInt(st.nextToken())-1);
 		}
-		
+
 		// 이제 술래 초기값 정해주고 바로 방향정해서 시뮬 돌리면 됨.
 		tagger = new Pair(N/2, N/2, 0);
-		
+
 		simulation();
-		
+
 		System.out.println(answer);
 	}
 
@@ -89,52 +90,68 @@ public class Main {
 		int move = 1;
 		int moveCnt = 0; // 2 가 될때마다 move를 1씩 올려줌
 		int cnt = 0;
-		int[][] dir = dxy;
-		
+		dir = dxy;
+		boolean rFlag = false;
+
 		for(int t = 1; t <= K; t++) {
 			moveThieves();
 
 			int nx = tagger.x + dir[tagger.d][0];
 			int ny = tagger.y + dir[tagger.d][1];
-			
-			if(nx < 0 || ny < 0 || nx >= N || ny >= N) {
-				dir = rdxy;
-				tagger.d = 0;
-				nx = tagger.x + dir[tagger.d][0];
-				ny = tagger.y + dir[tagger.d][1];
-			}
-			
+
 			tagger.x = nx;
 			tagger.y = ny;
 			
 			cnt++;
-			
-			
-			if(cnt == move) {
+			if(!rFlag && nx == 0 && ny == 0) {
+				rFlag = true;
+				dir = rdxy;
+				tagger.d = 0;
+				moveCnt = -1;
 				cnt = 0;
-				moveCnt++;
-				tagger.d = (tagger.d+1) % 4;
-				
-				if(moveCnt == 2) {
-					moveCnt = 0;
-					move += 1;
+				move = N-1;
+			}
+
+			else if(rFlag && nx == N/2 && ny == N/2) {
+				rFlag = false;
+				dir = dxy;
+				tagger.d = 0;
+				cnt = 0;
+				moveCnt = 0;
+				move = 1;
+			}
+			else {
+				if(cnt == move) {
+					cnt = 0;
+					moveCnt++;
+					tagger.d = (tagger.d+1) % 4;
+
+					if(moveCnt == 2) {
+						moveCnt = 0;
+						if(rFlag) move -= 1;
+						else move += 1;
+					}
 				}
 			}
-			
+
+
+
+
+
 			countScore(t);
-			
+
 		}
 	}	
-	
+
 	static void moveThieves() {
 		for(int i = 0; i < M; i++) {
 			if(thieves[i].d == -1) continue; // 잡힌 경우
 			if(thieves[i].getDistance(tagger.x, tagger.y) > 3) continue;
-			
+
 			int curX = thieves[i].x;
 			int curY = thieves[i].y;
 			int curD = thieves[i].d;
-			
+
 			int nx = curX + dxy[curD][0];
 			int ny = curY + dxy[curD][1];
 
@@ -142,31 +159,32 @@ public class Main {
 			if(nx < 0 || ny < 0 || nx >= N || ny >= N) {
 				thieves[i].d = (curD + 2) % 4;
 				curD = thieves[i].d;
-				
+
 				nx = curX + dxy[curD][0];
 				ny = curY + dxy[curD][1];
 			}
-			
+
 			// 다음위치에 술래가 있다면
 			if(tagger.getDistance(nx, ny) == 0) continue;
-			
+
 			// 술래가 없다면
 			thieves[i].x = nx;
 			thieves[i].y = ny;			
 		}
 	}
-	
+
 	static void countScore(int turn) {
 		int curD = tagger.d;
-		
+
 		for(int i = 0; i < 3; i++) { // 술래의 시야는 언제나 3
-			int nx = tagger.x + dxy[curD][0] * i;
-			int ny = tagger.y + dxy[curD][1] * i;
-			
+			int nx = tagger.x + dir[curD][0] * i;
+			int ny = tagger.y + dir[curD][1] * i;
+
 			if(nx < 0 || ny < 0 || nx >= N || ny >= N) continue;
-			
+
 			for(int j = 0; j < M; j++) { // 도망자 위치 체크
 				if(thieves[j].d == -1) continue;
+
 				if(thieves[j].getDistance(nx, ny) == 0 && !checkTreeHide(nx, ny)) {
 					thieves[j].d = -1;
 					answer += turn;
@@ -174,12 +192,12 @@ public class Main {
 			}
 		}
 	}
-	
+
 	static boolean checkTreeHide(int x, int y) {
 		for(int i = 0; i < H; i++) {
 			if(trees[i].x == x && trees[i].y == y) return true;
 		}
-		
+
 		return false;
 	}
 }
